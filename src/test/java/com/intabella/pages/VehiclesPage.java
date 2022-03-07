0f570c0e7dd2b77b5fe3417051ed84caa1fde64c
BETUL-BUYUKGOZE <betulcakmak23@hotmail.com>
2022-03-07 15:21:04 +0100

    Merge branch 'master' of I:\Simba4_Intabella with conflicts.

--- src/test/java/com/intabella/pages/VehiclesPage.java
+++ src/test/java/com/intabella/pages/VehiclesPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class VehiclesPage extends BasePage {
 
     @FindBy(css = "a.add-filter-button")
@@ -17,6 +19,25 @@ public class VehiclesPage extends BasePage {
     @FindBy(xpath = "//a[@title='Reset']")
     public WebElement resetButton;
 
+    @FindBy(xpath = "//div[@class='btn filter-criteria-selector oro-drop-opener oro-dropdown-toggle filter-default-value']")
+    public WebElement lastOdometerAllbtn;
+
+    @FindBy(xpath = "//div[@class='filter-item oro-drop open-filter']//button[@class='btn dropdown-toggle']")
+    public WebElement lastOdometerBetween;
+
+    @FindBy(xpath = "//span[@class='filter-items']/div[7]/div[2]//ul/li")
+    public List<WebElement> methodOptions;
+
+    @FindBy(xpath= "//div[@class='filter-start']/input")
+    public WebElement filterStart;
+
+    @FindBy(xpath = "//div[@class='filter-end']/input")
+    public WebElement filterEnd;
+
+    @FindBy(xpath = "//td[@class='number-cell grid-cell grid-body-cell grid-body-cell-LastOdometer']")
+    public List<WebElement> lastOdometerValues;
+
+
     public WebElement createLocator(String optionName) {
         return Driver.get().findElement(By.xpath( "//input[@value='" +optionName+ "']"));
     }
@@ -27,4 +48,10 @@ public class VehiclesPage extends BasePage {
             filterIcon.click();
         }
     }
+
+
+    public WebElement filterTypeLocators(String methodName) {
+        return Driver.get().findElement(By.xpath("//a[text()=\""+methodName+"\"]"));
+    }
+
 }
---
+++ src/test/java/com/intabella/step_definitions/LasOdometerStepDefs.java
@@ -0,0 +1,50 @@
+package com.intabella.step_definitions;
+
+import com.intabella.pages.VehiclesPage;
+import com.intabella.utilities.BrowserUtils;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LasOdometerStepDefs {
+    VehiclesPage vehiclesPage=new VehiclesPage();
+
+    @And("the user click on between button")
+    public void theUserClickOnBetweenButton() {
+        vehiclesPage.lastOdometerBetween.click();
+
+    }
+
+    @Then("Last Odometer filter should provide the {string}")
+    public void lastOdometerFilterShouldProvideThe(String methods) {
+
+        List<String> actualmethods = BrowserUtils.getElementsText(vehiclesPage.methodOptions);
+        Assert.assertEquals("the list doesn't contain "+methods+" option","["+methods+"]",actualmethods.toString());
+    }
+
+    @And("the user click on Last Odometer All button")
+    public void theUserClickOnLastOdometerAllButton() {
+        vehiclesPage.lastOdometerAllbtn.click();
+    }
+
+    @And("the user click on {string} method")
+    public void theUserClickOnMethod(String method) {
+        vehiclesPage.filterTypeLocators(method).click();
+        BrowserUtils.waitFor(2);
+    }
+
+    @Then("the results should be between the specified values")
+    public void theResultsShouldBeBetweenTheSpecifiedValues() {
+        String start ="1000";
+        String end = "3000";
+        vehiclesPage.filterStart.click();
+        BrowserUtils.waitFor(2);
+        vehiclesPage.filterStart.sendKeys(start);
+        vehiclesPage.filterEnd.click();
+        vehiclesPage.filterEnd.sendKeys(end);
+      //  List<String> actualmethods = BrowserUtils.get;
+    }
+}
