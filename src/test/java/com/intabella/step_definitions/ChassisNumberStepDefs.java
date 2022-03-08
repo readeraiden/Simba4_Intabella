ecba6271fff3bba8d3e34f068528fc3b56ba3adf
OnurEU <onur.eu7@gmail.com>
2022-03-08 22:38:03 +0100

    chassis number third commit

--- src/test/java/com/intabella/step_definitions/ChassisNumberStepDefs.java
+++ src/test/java/com/intabella/step_definitions/ChassisNumberStepDefs.java
@@ -1,19 +1,25 @@
 package com.intabella.step_definitions;
 
+import com.intabella.pages.DriverFilterPage;
 import com.intabella.pages.VehiclesPage;
 import com.intabella.utilities.BrowserUtils;
+import com.intabella.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class ChassisNumberStepDefs {
     VehiclesPage vehiclesPage=new VehiclesPage();
 
-    @When("the user click on Chassis Number All button")
-    public void the_user_click_on_Chassis_Number_All_button() {
-        vehiclesPage.chassisNumberAllbutton.click();
+    @When("the user click on All button")
+    public void theUserClickOnAllButton() {
+        vehiclesPage.Allbutton.click();
     }
 
     @Then("Chassis Number filter should provide the {string}")
@@ -23,4 +29,44 @@ public class ChassisNumberStepDefs {
         List<String> actualmethods = BrowserUtils.getElementsText(vehiclesPage.methodOptionsChassis);
         Assert.assertEquals("the list doesn't contain "+methods+" option","["+methods+"]",actualmethods.toString());
     }
-}
+
+
+    @And("the user enters {string} and {string}")
+    public void theUserEntersAnd(String value1, String value2) {
+
+        vehiclesPage.betweenValue1input.sendKeys(value1);
+        BrowserUtils.waitFor(2);
+        vehiclesPage.betweenValue2input.sendKeys(value2);
+        BrowserUtils.waitFor(2);
+        vehiclesPage.updateButtonn.click();
+        BrowserUtils.waitFor(2);
+
+    }
+
+    @Then("the results should be between the {string} and {string} in {string}")
+    public void theResultsShouldBeBetweenTheAndIn(String value1, String value2, String methodName) {
+        BrowserUtils.waitFor(4);
+        List<String> results = BrowserUtils.getElementsText(vehiclesPage.betweenValues(methodName));
+        for(String result : results){
+            result=result.replace(",","");
+            if(Integer.parseInt(result) >= Integer.parseInt(value1) && Integer.parseInt(result) <= Integer.parseInt(value2)){
+                BrowserUtils.waitFor(2);
+                Assert.assertTrue("the result must be equal or bigger than min filtered value",(Integer.parseInt(result)) >= Integer.parseInt(value1));
+                BrowserUtils.waitFor(2);
+                Assert.assertTrue("the result must be equal or less than max filtered value",(Integer.parseInt(result)) <= Integer.parseInt(value2));
+            }
+            System.out.println(result);
+        }
+    }
+
+    @When("the user select {string}")
+    public void theUserSelect(String method) {
+
+        vehiclesPage.filterTypeLocators(method).click();
+        BrowserUtils.waitFor(2);
+        }
+    }
+
+
+
+
--- src/test/resources/features/chassisNumber.feature
+++ src/test/resources/features/chassisNumber.feature
@@ -12,11 +12,24 @@ Feature: Chassis Number Filter
     Given the user click on the filter button
     When the user click on manage filter button
     When the user click on "ChassisNumber"
-    And the user click on Chassis Number All button
+    And the user click on All button
     And the user click on between button
     Then Chassis Number filter should provide the "<methods>"
     Examples:
       | methods                                                                                                                            |
       | Between, Not Between, Equals, Not Equals, More Than, Less Than, Equals Or More Than, Equals Or Less Than, Is Empty, Is Not Empty |
 
+  Scenario Outline:When the user selects the "<method>" with numeric values,the results should be between the specified values
+    Given the user click on the filter button
+    When the user click on manage filter button
+    When the user click on "ChassisNumber"
+    And the user click on All button
+    And the user click on between button
+    When the user select "<method>"
+    And the user enters "<value1>" and "<value2>"
+    Then the results should be between the "<value1>" and "<value2>" in "Chassis Number"
+
+    Examples:
+      | method | value1 | value2|
+      |between|    3000 |200000 |
 
