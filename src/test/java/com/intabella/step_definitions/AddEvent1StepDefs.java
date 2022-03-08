ea60e7303669327c7f2b523ee3ce082115535893
ahaydogmus <aydogmus.fr@gmail.com>
2022-03-08 12:30:42 +0100

    AddEvent Page, AddEvent1StepDefs, addEvent.feature Added

--- src/test/java/com/intabella/step_definitions/AddEvent1StepDefs.java
+++ src/test/java/com/intabella/step_definitions/AddEvent1StepDefs.java
@@ -1,58 +1,101 @@
 package com.intabella.step_definitions;
 
-import com.intabella.pages.CreateCarPage;
-import com.intabella.pages.VehiclesPage;
+import com.intabella.pages.AddEventPage;
+import com.intabella.utilities.BrowserUtils;
+import com.intabella.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddEvent1StepDefs {
-    CreateCarPage createCarPage=new CreateCarPage();
-
+    AddEventPage addEventPage=new AddEventPage();
 
     @Given("User clicks on any vehicle")
-    public void userClicksOnAnyVehicle() {
-        createCarPage.row2.click();
+    public void userClicksOnAnyVehicle() throws InterruptedException {
+        BrowserUtils.waitForPageToLoad(5);
+        BrowserUtils.waitForVisibility(addEventPage.oneVehicleToClick,20);
+        addEventPage.oneVehicleToClick.click();
+        Assert.assertTrue(addEventPage.generalInfo.isDisplayed());
+        BrowserUtils.waitForPageToLoad(5);
     }
-
-    @When("User clicks on {string} button")
-    public void user_clicks_on_button(String string) {
-
+    @When("User clicks on Add Event button")
+    public void userClicksOnAddEventButton() throws InterruptedException {
+        Thread.sleep(3000);
+        addEventPage.addEventButton.click();
+        try {
+            new WebDriverWait(Driver.get(), 5).until(ExpectedConditions.elementToBeClickable(addEventPage.addEventButton));
+            Assert.assertTrue(true);
+            System.out.println("Add Event button is clickable");
+        } catch (Exception e) {
+            Assert.assertFalse(false);
+            System.out.println("Add Event button isn't clickable");
+        }
     }
+    @Then("the Add Event page should pop up")
+    public void theAddEventPageShouldPopUp() {
 
-    @Then("the {string} page should pop up")
-    public void the_page_should_pop_up(String string) {
+        try {
+        new WebDriverWait(Driver.get(), 5).until(ExpectedConditions.visibilityOf(addEventPage.addEventPopup));
+            Assert.assertTrue(addEventPage.addEventPopup.isDisplayed());
+            System.out.println("Add Event Page is popped up");
+        }
+        catch(TimeoutException e) {
+            System.out.println("Add Event Page isn't popped up");
+        }}
+    @And("Compulsory fields should be as below")
+    public void compulsoryFieldsShouldBeAsBelow(List<String> compulsoryField) {
+        BrowserUtils.waitFor(3);
+      List<String> allLabels = BrowserUtils.getElementsText(addEventPage.labels);
+        System.out.println("allLabels.size() = " + allLabels.size());
+        List<String> allLabelsNoStar= new ArrayList<>();
 
+        for (String allLabel : allLabels) {
+            allLabelsNoStar.add(allLabel.substring(0,allLabel.length()-1));
+        }
+        System.out.println("compulsory fields = " + compulsoryField);
+            Assert.assertTrue(allLabelsNoStar.containsAll(compulsoryField));
+        }
+    @And("If any compulsory field is not filled")
+    public void if_any_compulsory_field_is_not_filled() {
+        addEventPage.titleInputBox.sendKeys(" ");
+        addEventPage.organizerDisplayNameInputBox.sendKeys("Mrs. Jane");
     }
-
-    @Then("Compulsory fields should be seen as below")
-    public void compulsory_fields_should_be_seen_as_below(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-
-    }
-
     @When("User clicks on the save button")
     public void user_clicks_on_the_save_button() {
-
+        addEventPage.saveButton.click();
     }
-
-    @When("If any compulsory field is not filled")
-    public void if_any_compulsory_field_is_not_filled() {
-
-
-    }
-
     @Then("{string} message should be displayed")
-    public void message_should_be_displayed(String string) {
+    public void message_should_be_displayed(String message) throws InterruptedException {
+        Assert.assertEquals(message,addEventPage.blankFieldwarningMessage.getText());
+        System.out.println("warningMessage = " + addEventPage.blankFieldwarningMessage.getText());
+
+        addEventPage.organizerDisplayNameInputBox.clear();
+        addEventPage.organizerDisplayNameInputBox.sendKeys("Microsoft");
+        addEventPage.organizerEmailInputBox.sendKeys("whattodo@gma.co");
+        addEventPage.saveButton.click();
+        Thread.sleep(2000);
+        Assert.assertEquals(message, addEventPage.blankFieldwarningMessage.getText());
+
+        addEventPage.titleInputBox.sendKeys("SDET");
+        addEventPage.saveButton.click();
+        Thread.sleep(2000);
+        Assert.assertNotEquals("Verify that event shouldn't be saved", "Calendar event saved", addEventPage.calendarEventAddedPopup.getText());
+        System.out.println("calendarEventAddedPopup = " + addEventPage.calendarEventAddedPopup.getText());
+
+
 
 
     }
 
+    }
 
-}
+
+
--- src/test/resources/features/addEvent1.feature
+++ src/test/resources/features/addEvent1.feature
@@ -1,16 +1,36 @@
-Feature: Add Event Function 1
-  @wip
-  Scenario: Add Event Functionality for Store Manager
-    Given User clicks on any vehicle
-    When User clicks on "Add Event" button
-    Then the "Add Event" page should pop up
-    And Compulsory fields should be seen as below
-      | Title                  |
-      | Owner                  |
-      | Organizer display name |
-      | Organizer email        |
-      | Start Date             |
-      | End Date               |
-    When User clicks on the save button
-    And If any compulsory field is not filled
-    Then "This value should not be blank." message should be displayed
+
+Feature: Add Event Functionality - Store Manager
+	@INTA-1165 @INTA-1163
+	Scenario: US005-AC1-Verify that Store Manager can access "General Information" page by clicking on any vehicle/row
+		Given User clicks on any vehicle	
+
+	@INTA-1166 @INTA-1163
+	Scenario: US005-AC2-Verify that Store Manager can click the "Add Event" button
+		Given User clicks on any vehicle
+		Then User clicks on Add Event button	
+
+	@INTA-1167 @INTA-1163
+	Scenario: US005-AC3-Verify that "Add Event" page is popped up after clicking on the "Add event" button
+		Given User clicks on any vehicle
+		When User clicks on Add Event button
+		Then the Add Event page should pop up	
+
+	@INTA-1168 @INTA-1163
+	Scenario: US005-AC4-Verify that Compulsory fields are shown
+		Given User clicks on any vehicle
+		    When User clicks on Add Event button
+		    Then  Compulsory fields should be as below
+		      | Title                  |
+		      | Owner                  |
+		      | Organizer display name |
+		      | Organizer email        |
+		      | Start                  |
+		      | End                    |	
+
+	@INTA-1169 @INTA-1163
+	Scenario: US005-AC5-Verify that "This value should not be blank." message is displayed after clicking on the save button If any compulsory field is not filled
+		Given User clicks on any vehicle
+		    When User clicks on Add Event button
+		    And If any compulsory field is not filled
+		    When User clicks on the save button
+		    Then "This value should not be blank." message should be displayed
